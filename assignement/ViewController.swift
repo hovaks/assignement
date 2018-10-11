b7c4c290ebd84234e7575097248d51c1de28a8cc
Hovak Davtyan <hovaks@gmail.com>
2018-10-11 20:43:37 +0400

    Initial Commit

---
+++ assignement/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  assignement
+//
+//  Created by Hovak Davtyan on 10/11/18.
+//  Copyright © 2018 Hovak Davtyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
